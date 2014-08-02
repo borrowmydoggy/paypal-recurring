6d08dbd14a140f40a4b5554db760d06b429907a6
Emir Ibrahimbegovic <emir.ibrahimbegovic@gmail.com>
2014-08-01 21:27:43 -0400

    Resolving synthax error, tests pass again

--- lib/paypal/recurring/response/payment.rb
+++ lib/paypal/recurring/response/payment.rb
@@ -3,13 +3,13 @@ module PayPal
     module Response
       class Payment < Base
         mapping(
-          :status                 => :PAYMENTINFO_0_PAYMENTSTATUS,
-          :amount                 => :PAYMENTINFO_0_AMT,
-          :fees                   => :PAYMENTINFO_0_FEEAMT,
-          :transaction_id         => :PAYMENTINFO_0_TRANSACTIONID,
-          :seller_id              => :PAYMENTINFO_0_SECUREMERCHANTACCOUNTID,
-          :reference              => [:PROFILEREFERENCE, :PAYMENTREQUEST_0_CUSTOM, :PAYMENTREQUEST_0_INVNUM]
-          :billing_agreement_id   => :BILLINGAGREEMENTID
+          :status               => :PAYMENTINFO_0_PAYMENTSTATUS,
+          :amount               => :PAYMENTINFO_0_AMT,
+          :fees                 => :PAYMENTINFO_0_FEEAMT,
+          :transaction_id       => :PAYMENTINFO_0_TRANSACTIONID,
+          :seller_id            => :PAYMENTINFO_0_SECUREMERCHANTACCOUNTID,
+          :billing_agreement_id => :BILLINGAGREEMENTID,
+          :reference            => [:PROFILEREFERENCE, :PAYMENTREQUEST_0_CUSTOM, :PAYMENTREQUEST_0_INVNUM]
         )
 
         def completed?
