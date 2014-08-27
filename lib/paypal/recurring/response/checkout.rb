5a5704a1ad5a85907e0407e8d9471cb396f37be7
Emir Ibrahimbegovic <emir.ibrahimbegovic@gmail.com>
2014-08-26 22:38:37 -0400

    When token is supplied get the redirect url

--- lib/paypal/recurring/response/checkout.rb
+++ lib/paypal/recurring/response/checkout.rb
@@ -5,6 +5,10 @@ module PayPal
         def checkout_url
           "#{PayPal::Recurring.site_endpoint}?cmd=_express-checkout&token=#{token}&useraction=commit"
         end
+
+        def redirect_back_to_paypal_url(token)
+          "#{PayPal::Recurring.site_endpoint}?cmd=_express-checkout&token=#{token}"
+        end
       end
     end
   end
--- spec/paypal/response/checkout_spec.rb
+++ spec/paypal/response/checkout_spec.rb
@@ -20,6 +20,10 @@ describe PayPal::Recurring::Response::Checkout do
     its(:valid?) { should be_true }
     its(:errors) { should be_empty }
     its(:checkout_url) { should == "#{PayPal::Recurring.site_endpoint}?cmd=_express-checkout&token=EC-6K296451S2213041J&useraction=commit" }
+
+    it "should retur right url with paramaterized token" do
+      subject.redirect_back_to_paypal_url('EC-6K22').should == "#{PayPal::Recurring.site_endpoint}?cmd=_express-checkout&token=EC-6K22"
+    end
   end
 
   context "when failure" do
